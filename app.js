6b68018c4a2655d2e2c1f3c275daf804b582e1c1
alagiz <artem.alagizov@gmail.com>
2022-03-03 10:37:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import {bech32} from 'bech32';
+import web3 from 'web3-utils';
 
+const {hexToBytes} = web3;
 const terraAddress = "";
+const inputData = "";
 
 const decodeTerraAddressOnEtherBase = address => {
   try {
@@ -13,8 +16,22 @@ const decodeTerraAddressOnEtherBase = address => {
   }
 };
 
-console.log("started decoding", terraAddress);
+const getTerraAddressFromInputData = inputData => {
+  try {
+    return bech32.encode(
+      'terra',
+      bech32.toWords(hexToBytes(inputData.slice(0, 42))));
+  } catch (error) {
+    console.log("error while getTerraAddressFromInputData:", error);
+    return '';
+  }
+};
 
-const decoded = decodeTerraAddressOnEtherBase(terraAddress);
+// console.log("started decoding", terraAddress);
+//
+// const decoded = decodeTerraAddressOnEtherBase(terraAddress);
+//
+// console.log("result:", !!decoded ? decoded.padEnd(66, '0') : "unsuccessful");
 
-console.log("result:", !!decoded ? decoded.padEnd(66, '0') : "unsuccessful");
+console.log("getTerraAddressFromInputData");
+console.log(getTerraAddressFromInputData(inputData));
